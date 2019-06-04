dc253c9743131ef30ac70bb9f766cd077f8a947f
gabrieldansereau <gabriel.dansereau@hotmail.com>
2019-06-04 16:15:00 -0400

    dataset operations

--- visualization.jl
+++ visualization.jl
@@ -1,9 +1,27 @@
 using CSV
-# using DataFrames
+using DataFrames
 # using JuliaDB
-
-CSV.read("../data/warblers_mtl.csv", header=false, delim="\t")
-
 using SimpleSDMLayers
+# using Statistics
 
+# Read csv
+warblers = CSV.read("../data/warblers_mtl.csv", header=true, delim="\t")
+# Dataframe names
+names(warblers)
+# Specific column
+warblers.species
+df = warblers[:, [:species, :year, :decimalLatitude, :decimalLongitude]]
+df
+# First entries (~head)
+first(df,6)
+# Show all columns
+show(first(df,6), allcols=true)
+# Describe columns (~summary)
+show(describe(df), allcols=true)
+# Select on conditions
+df_full = dropmissing(df, :year)
+df_full[df_full.year .< 1900, :]
+show(warblers[warblers.year .=== 1700, :], allcols=true)
+
+# Bioclim layers
 temperature, precipitation = worldclim([1,12])
