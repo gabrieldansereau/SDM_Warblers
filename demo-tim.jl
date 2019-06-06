e08b800b518c66e3c5eea7143734541cc9717d63
gabrieldansereau <gabriel.dansereau@hotmail.com>
2019-06-06 14:33:30 -0400

    comment Tim's demo

--- demo-tim.jl
+++ demo-tim.jl
@@ -1,8 +1,6 @@
-using Pkg
-Pkg.add https://github.com/EcoJulia/SimpleSDMLayers.jl#master
-Pkg.add Plots
 using SimpleSDMLayers
 using Plots
+# Get bioclim variables
 temperature, precipitation = worldclim([1,12])
 temperature
 temperature.grid
@@ -11,14 +9,24 @@ temperature.right
 temperature.top
 temperature.bottom
 temperature[47.0, -12.0]
+
+# Global heatmap
 heatmap(temperature.grid)
 temperature[800, 500]
+# No heat values in oceans
+
+# Heatmap with longitudes & latitudes
+longitudes(temperature)
 heatmap(longitudes(temperature), latitudes(temperature), temperature.grid)
 temperature[-100.0, 30.0]
-precipitations[-100.0, 30.0]
 precipitation[-100.0, 30.0]
+
+# Restrict latitudes to NA
 temperature[(-160.0, -50.0), (0.0, 90.0)]
 temperature[(-160.0, -50.0), (0.0, 90.0)] |> x -> heatmap(x.grid)
 temp_NA = temperature[(-160.0, -50.0), (0.0, 90.0)]
+
+# Plot precipitations according to precipitation grid
 precipitation[temp_NA]
 precipitation[temp_NA] |> x -> heatmap(x.grid)
+precipitation[temperature] |> x -> heatmap(x.grid)
