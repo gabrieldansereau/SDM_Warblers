66d3802b3afd6431d6eb591077fdb04d8bd9acf7
gabrieldansereau <gabriel.dansereau@hotmail.com>
2019-07-02 11:51:16 -0400

    wrap extraction to dataframe in function

--- src/data_wc-vars-df.jl
+++ src/data_wc-vars-df.jl
@@ -5,28 +5,32 @@ using SimpleSDMLayers
 var_names = (:temperature, :precipitation)
 wc_vars = [worldclim(i, resolution="2.5") for i in (1,12)]
 
-# Extract variable dimensions
-grid_size_lat = size(wc_vars[1].grid)[1]
-grid_size_long = size(wc_vars[1].grid)[2]
-grid_size_total = length(wc_vars[1].grid)
+function wc_vars_df(vars, names)
+    # Extract variable dimensions
+    grid_size_lat = size(vars[1].grid)[1]
+    grid_size_long = size(vars[1].grid)[2]
+    grid_size_total = length(vars[1].grid)
 
-# Determine ratio array cells per lat/long degree
-grid_ratio = grid_size_lat/(2*90)
-grid_ratio == grid_size_long/(2*180) # must be true
+    # Determine ratio array cells per lat/long degree
+    grid_ratio = grid_size_lat/(2*90)
+    grid_ratio == grid_size_long/(2*180) # must be true
 
-# Create dataframe for keep observations
-clim = DataFrame()
+    # Create dataframe for keep observations
+    clim = DataFrame()
 
-# Coordinates range
-lats = collect(wc_vars[1].bottom:1/grid_ratio:(wc_vars[1].top-1/grid_ratio))
-longs = collect(wc_vars[1].left:1/grid_ratio:(wc_vars[1].right-1/grid_ratio))
+    # Coordinates range
+    lats = collect(vars[1].bottom:1/grid_ratio:(vars[1].top-1/grid_ratio))
+    longs = collect(vars[1].left:1/grid_ratio:(vars[1].right-1/grid_ratio))
 
-# Fill df with coordinates
-clim.latitude = repeat(lats, outer = grid_size_long)
-clim.longitude = repeat(longs, inner = grid_size_lat)
+    # Fill df with coordinates
+    clim.latitude = repeat(lats, outer = grid_size_long)
+    clim.longitude = repeat(longs, inner = grid_size_lat)
 
-# Extract climate data
-for i in 1:length(wc_vars)
-    clim[var_names[i]] = vec(wc_vars[i].grid)
+    # Extract climate data
+    for i in 1:length(vars)
+        clim[var_names[i]] = vec(vars[i].grid)
+    end
+
+    return clim
 end
-clim
+wc_vars_df(wc_vars, var_names)
