9d05bb24908aa898078547dda2223a22936e8329
gabrieldansereau <gabriel.dansereau@hotmail.com>
2019-07-02 16:41:21 -0400

    use functions latitudes() & longitudes()

--- src/data_wc-vars-df.jl
+++ src/data_wc-vars-df.jl
@@ -6,21 +6,16 @@ var_names = (:temperature, :precipitation)
 wc_vars = [worldclim(i, resolution="2.5") for i in (1,12)]
 
 function wc_vars_df(vars, names)
-    # Extract variable dimensions
-    grid_size_lat = size(vars[1].grid)[1]
-    grid_size_long = size(vars[1].grid)[2]
-    grid_size_total = length(vars[1].grid)
+    # Extract array dimensions
+    grid_size_lat = size(vars[1], 1)
+    grid_size_long = size(vars[1], 2)
 
-    # Determine ratio array cells per lat/long degree
-    grid_ratio = grid_size_lat/(2*90)
-    grid_ratio == grid_size_long/(2*180) # must be true
-
-    # Create dataframe for keep observations
+    # Create dataframe to keep observations
     clim = DataFrame()
 
     # Coordinates range
-    lats = collect(vars[1].bottom:1/grid_ratio:(vars[1].top-1/grid_ratio))
-    longs = collect(vars[1].left:1/grid_ratio:(vars[1].right-1/grid_ratio))
+    lats = collect(latitudes(vars[1]))
+    longs = collect(longitudes(vars[1]))
 
     # Fill df with coordinates
     clim.latitude = repeat(lats, outer = grid_size_long)
