2901f407bcebc4a78055ca8d3157e2f0e75c0e7a
gabrieldansereau <gabriel.dansereau@hotmail.com>
2019-07-03 14:50:30 -0400

    minor fix

--- src/functions_coordinates.jl
+++ src/functions_coordinates.jl
@@ -7,13 +7,13 @@ function conv_long(long, grid_ratio)
 end
 
 # Custom functions to round coordinates
-function coord_floor(coord, grid_ratio)
+function coord_floor(coord)
     floor(coord*grid_ratio)/grid_ratio
 end
-function coord_ceil(coord, grid_ratio)
+function coord_ceil(coord)
     ceil(coord*grid_ratio)/grid_ratio
 end
-function coord_round(coord, grid_ratio)
+function coord_round(coord)
     round(coord*grid_ratio)/grid_ratio
 end
 function coord_range(coords, grid_ratio)
